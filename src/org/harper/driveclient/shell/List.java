6d47441861314a9f8e5c3425b7738babd9fd5cd2
Hao Jiang <harperjiang@msn.com>
2017-08-05 22:49:06 -0500

    display only files not trashed, display created date

--- src/org/harper/driveclient/shell/List.java
+++ src/org/harper/driveclient/shell/List.java
@@ -1,7 +1,9 @@
 package org.harper.driveclient.shell;
 
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
 
 import org.harper.driveclient.Shell;
 
@@ -16,10 +18,13 @@ public class List extends Command {
 		Drive d = shell.getDrive();
 		ChildList children = d.children().list(shell.getCurrentFolder()).execute();
 
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		children.getItems().forEach((ChildReference c) -> {
 			try {
 				File f = d.files().get(c.getId()).execute();
-				System.out.println(MessageFormat.format("{0}:{1}", f.getTitle(), f.getId()));
+				if (!f.getLabels().getTrashed()) {
+					System.out.println(MessageFormat.format("{0}\t{1}", f.getTitle(), df.format(f.getCreatedDate())));
+				}
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
